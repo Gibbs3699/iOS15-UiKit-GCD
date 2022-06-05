8f65c814aa8ef86308097797f8f063370ca70862
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-05 23:22:10 +0700

    Rendering a petition: loadHTMLString

---
+++ Whitehouse Petitions/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by TheGIZzz on 5/6/2565 BE.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var detailItem: Petition?
+
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else {
+            return
+        }
+
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+
+}
